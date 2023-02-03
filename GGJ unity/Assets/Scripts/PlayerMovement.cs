e2e877daa920e90886cf0fccac16afa0de17213c
Sean R. Tan <SeanRonanTan.2003@gmail.com>
2023-02-03 13:26:18 +0000

    fully working player controls
    tracer ability next

---
+++ GGJ unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float Horz;
+    private float speed = 7f;
+    private float JumpingPower = 11f;
+    private bool IsFacingRight = true;
+
+    [SerializeField] private Rigidbody2D RB;
+    [SerializeField] private Transform FloorCheck;
+    [SerializeField] private LayerMask FloorLayer;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horz = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            RB.velocity = new Vector2(RB.velocity.x, JumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && RB.velocity.y > 0f)
+        {
+            RB.velocity = new Vector2(RB.velocity.x, RB.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        RB.velocity = new Vector2(Horz * speed, RB.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(FloorCheck.position, 0.2f, FloorLayer);
+
+    }
+
+    private void Flip()
+    {
+        if (IsFacingRight && Horz < 0f || !IsFacingRight && Horz > 0f)
+        {
+            IsFacingRight = !IsFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+}
